10cf051d0ea3834ca8f16e40a1d6dd259a41e156
ken <ken0638@cchntek.com>
2019-12-26 17:56:39 +0800

    [fix]

---
+++ src/ApiKey.js
@@ -0,0 +1,11 @@
+
+export default {
+    apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+    authDomain: "cutoff-a0518.firebaseapp.com",
+    databaseURL: "https://cutoff-a0518.firebaseio.com",
+    projectId: "cutoff-a0518",
+    storageBucket: "cutoff-a0518.appspot.com",
+    messagingSenderId: "192068221825",
+    appId: process.env.REACT_APP_MESSAGINGSENDERID,
+    measurementId: "G-HGYC9DH7KR"
+  };
