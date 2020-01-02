fd4827c439129514416df1d6b8414454a8a577ea
ken <ken0638@cchntek.com>
2020-01-02 18:07:16 +0800

    [fix]img deploy

--- src/component/pages/Login.js
+++ src/component/pages/Login.js
@@ -4,7 +4,7 @@ import { withRouter } from "react-router-dom"
 
 import { FormControl, TextField, Button, } from '@material-ui/core';
 import { ContextStore } from "reducer"
-import bgImg from 'Img/login.jpeg'
+// import bgImg from 'Img/login.jpeg'
 import { apiLogin } from "Api"
 // util
 import {abstractAccount} from "Utils/tools"
@@ -13,7 +13,7 @@ import {abstractAccount} from "Utils/tools"
 const StyledLogin = styled.div`
 width:100%;
 height: 100%;
-background-image:url(${bgImg});
+background-image:url(${require('Img/login.jpeg')}});
 background-repeat: no-repeat;
 background-size: cover;
 padding-top:80px;
