7c71b9e784dee15386a6a1d51cd9d082ad1d3aab
ken <ken0638@cchntek.com>
2020-01-03 09:37:08 +0800

    [fix]deploy

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useReducer } from "react";
 import { HashRouter } from "react-router-dom";
 
 // components
-import Routes from "./Router";
+import Routes from "./router";
 
 //reducer
 import { reducer, initialState, ContextStore } from 'reducer';
